1f6204b5f813f57a7fde0fe73c8571871d650bba
Anubhav Mishra <anubhavm96@gmail.com>
2021-10-09 12:52:42 +0530

    updated urls

--- API_base/urls.py
+++ API_base/urls.py
@@ -5,11 +5,11 @@ from rooms.views import list_rooms,room_detail,add_room
 
 
 urlpatterns = [
-    path('patient-list/',list_patients,name='List_Patients'),
+    path('patient-list',list_patients,name='List_Patients'),
     path('patient-list/<int:pk>',patient_detail,name='Patient_Detail'),
-    path('doctor-list/',list_doctors,name='List_Doctors'),
+    path('doctor-list',list_doctors,name='List_Doctors'),
     path('doctor-list/<int:pk>',doctor_detail,name='Doctor_Detail'),
-    path('room-list/',list_rooms,name='List_Rooms'),
+    path('room-list',list_rooms,name='List_Rooms'),
     path('room-list/<int:pk>',room_detail,name='Room_Detail'),
     path('add-room',add_room,name='Add_Room'),
     path('add-patient',add_patient,name='Add_Patient'),
