dca1ff32296d1fae849455eddf62b64548b46f2b
Anubhav Mishra <anubhavm96@gmail.com>
2021-10-21 08:48:41 +0530

    minor changes

--- manager_api/settings.py
+++ manager_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bs*f#)%=#($gnle&t-#x65j^$zxk)bao@j$&&rp*^2okt1*zzj
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anumish.pythonanywhere.com']
 
 
 # Application definition
